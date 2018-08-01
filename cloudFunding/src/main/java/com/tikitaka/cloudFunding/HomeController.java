36d7957755c92e4279ec692955ffb65908b30db7
sini <user2@user2-PC>
2018-08-01 20:06:53 +0900

    나라라

--- cloudFunding/src/main/java/com/tikitaka/cloudFunding/HomeController.java
+++ cloudFunding/src/main/java/com/tikitaka/cloudFunding/HomeController.java
@@ -34,6 +34,7 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		System.out.println("test");
 		System.out.println("없어저라");
+		System.out.println("없어져라2");
 		return "home";
 	}
 	
