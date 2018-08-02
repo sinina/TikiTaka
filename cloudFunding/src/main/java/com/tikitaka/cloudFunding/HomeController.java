9d59f728a5c5299ee7217af426e037fa791f772c
sini <user2@user2-PC>
2018-08-02 16:54:46 +0900

    asd

--- cloudFunding/src/main/java/com/tikitaka/cloudFunding/HomeController.java
+++ cloudFunding/src/main/java/com/tikitaka/cloudFunding/HomeController.java
@@ -35,7 +35,7 @@ public class HomeController {
 		System.out.println("sd");
 		System.out.println("123");
 		System.out.println("111");
-		System.out.println("222");
+		System.out.println("444");
 
 		return "home";
 	}
