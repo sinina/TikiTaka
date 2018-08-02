b2fc89e3372c7e35f8b74d0f19aae54666c21c99
YSJ <ysjhmtb@gmail.com>
2018-08-02 20:00:41 +0900

    멤버 페이지와 폴더 변

---
+++ cloudFunding/src/main/java/com/tikitaka/cloudFunding/member/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.tikitaka.cloudFunding.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	
+	@RequestMapping("loginPage.do")
+	public String loginPage() {
+		return "member/loginPage";
+	}
+	
+	@RequestMapping("joinPage.do")
+	public String joinPage() {
+		return "member/joinPage";
+				
+	}
+}
