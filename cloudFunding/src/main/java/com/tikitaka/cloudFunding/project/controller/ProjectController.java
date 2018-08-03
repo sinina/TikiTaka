41aecf6a7837bc5c73778e23d94774e8ec1764f4
wjddl <wjddl@LAPTOP-8TG7QDUP>
2018-08-03 20:29:02 +0900

    프로젝트 둘러보기 화면 추가

--- cloudFunding/src/main/java/com/tikitaka/cloudFunding/project/controller/ProjectController.java
+++ cloudFunding/src/main/java/com/tikitaka/cloudFunding/project/controller/ProjectController.java
@@ -1,13 +1,26 @@
 package com.tikitaka.cloudFunding.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProjectController {
+	/*@Autowired
+	ProjectService pService;*/
 	
 	@RequestMapping("projectStart.do")
 	public String ProjectForm(){
 		return "project/projectstart";
 	}
+	
+	/*@RequestMapping("projectDetail.do")
+	public ModelAndView projectDetail(int pProjectCode, ModelAndView mv){
+		ProjectVo project = pService.selectProject(pProjectCode);
+		mv.addObject("project", project);
+		mv.setViewName("project/projectDetail");
+		return mv;
+		
+	}*/
 }
